6d294e8c422afd865d02c64c26a10de55c53f916
root <root@smtpout1.eu.geekinbox.com>
2015-06-30 13:04:30 +0000

    npm++

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 
 var expect = require('chai').expect
 var DynamoDB = require('aws-dynamodb')()
+DynamoDB.on('error', function(op, error, payload ) {
+	//console.log(op,error,payload)
+})
+DynamoDB.on('beforeRequest', function(op, payload ) {
+	//console.log("--------------------------------")
+	//console.log(op,payload)
+})
 var $tableName = 'test_hash_range'
 
 describe('client.deleteTable (test_hash_range)', function () {
