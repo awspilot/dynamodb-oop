07ad6cbc195fc840f06469201f2f695cf32f4f7b
databank <adrian@databank.ro>
2016-08-27 14:08:30 +0300

    query: select() and filter() allow wrapping of attribute names into double quotes

--- test/tests/020-specialSigns.js
+++ test/tests/020-specialSigns.js
@@ -93,7 +93,14 @@ describe('special signs in attribute names', function () {
 				range: 1234,
 				'account.id': "aaa",
 				'non.key.attribute': 1,
-				'nested.object': { 'nested.attribute': 2 }
+				'nested.object': { 'nested.attribute': 2 },
+				nested: {
+					object: {
+						nested: {
+							attribute: 3
+						}
+					}
+				}
 			}, function(err, data) {
 				if (err)
 					throw err
@@ -119,18 +126,60 @@ describe('special signs in attribute names', function () {
 				assert.equal(data.length,1)
 				assert.equal(data[0]['account.id'],'aaa')
 				assert.equal(data[0]['nested.object']['nested.attribute'],2)
+				assert.equal(data[0].nested.object.nested.attribute, 3 )
+				done()
+			})
+	})
+
+	// allow "." in filter by wrapping attribute name in ""
+	it('should allow attributes with "." sign in filter()', function(done) {
+		DynamoDB
+			.table($tableName)
+			.index('byAccount.Id')
+			.where('account.id').eq( 'aaa' )
+			.having('"non.key.attribute"').gt(0)
+			.having('"nested.object"."nested.attribute"').eq(2)
+			.having('nested.object.nested.attribute').eq(3)
+			.on('beforeRequest', function(op, payload) {
+				//console.log(op, JSON.stringify(payload,null,"\t"))
+			})
+			.query(function(err, data ) {
+				if (err)
+					throw err
+
+				assert.equal(data.length,1)
+				assert.equal(data[0]['non.key.attribute'],1)
+				assert.equal(data[0]['nested.object']['nested.attribute'],2)
+				assert.equal(data[0].nested.object.nested.attribute, 3 )
+				//console.log(JSON.stringify(data, null, "\t"))
 
 				done()
 			})
 	})
 
-	// @todo: allow "." in filter by wrapping attribute name in ``
-	//it('should allow attributes with "." sign in filter()', function(done) {
-	//})
 
+	// allow "." in select by wrapping attribute name in ""
+	it('should allow attributes with "." sign in select()', function(done) {
+		DynamoDB
+			.table($tableName)
+			.select('"non.key.attribute"','"nested.object"."nested.attribute"', 'nested.object.nested.attribute')
+			.index('byAccount.Id')
+			.descending()
+			.where('account.id').eq( 'aaa' )
+			.on('beforeRequest', function(op, payload) {
+				//console.log(op, JSON.stringify(payload,null,"\t"))
+			})
+			.query(function(err, data ) {
+				if (err)
+					throw err
 
-	// @todo: allow "." in select by wrapping attribute name in ``
-	//it('should allow attributes with "." sign in select()', function(done) {
-	//})
+				assert.equal(data.length,1)
+				assert.equal(data[0]['non.key.attribute'],1)
+				assert.equal(data[0]['nested.object']['nested.attribute'],2)
+				assert.equal(data[0].nested.object.nested.attribute, 3 )
+				//console.log(JSON.stringify(data, null, "\t"))
+				done()
+			})
+	})
 
 })
