682fcd97a805c148919aaf76d18b07c4ab7e3dce
databank <adrian@databank.ro>
2016-11-27 12:33:12 +0200

    buffer support

--- test/tests/030-insert.js
+++ test/tests/030-insert.js
@@ -101,8 +101,6 @@ describe('insert', function () {
 						gsi_range: 'a',
 						array: [1,2,3],
 						object: {aaa:1,bbb:2, ccc:3, ddd: {ddd1: 1}, eee: [1,'eee1']},
-						string_set: DynamoDB.stringSet(['aaa','bbb','ccc']),
-						number_set: DynamoDB.numberSet([111,222,333]),
 					}, function(err, data) {
 						if (err)
 							throw err
@@ -115,7 +113,6 @@ describe('insert', function () {
 								if (err)
 									throw err
 
-								item.number_set.sort()
 								assert.deepEqual(item, {
 									hash: 'hash1',
 									range: 1,
@@ -124,7 +121,76 @@ describe('insert', function () {
 									gsi_range: 'a',
 									array: [1,2,3],
 									object: {aaa:1,bbb:2, ccc:3, ddd: {ddd1: 1}, eee: [1,'eee1']},
+								}, {strict: true })
+								done()
+							})
+					})
+			})
+	})
+
+	it('insert stringset', function(done) {
+		DynamoDB
+			.table($tableName)
+			.where('hash').eq('hash_test_stringset')
+			.where('range').eq(1)
+			.delete(function( err, data ) {
+				DynamoDB
+					.table($tableName)
+					.insert({
+						hash: 'hash_test_stringset',
+						range: 1,
+						string_set: DynamoDB.stringSet(['aaa','bbb','ccc']),
+					}, function(err, data) {
+						if (err)
+							throw err
+
+						DynamoDB
+							.table($tableName)
+							.where('hash').eq('hash_test_stringset')
+							.where('range').eq(1)
+							.get(function(err, item) {
+								if (err)
+									throw err
+
+								assert.deepEqual(item, {
+									hash: 'hash_test_stringset',
+									range: 1,
 									string_set: ['aaa','bbb','ccc'],
+								}, {strict: true })
+								done()
+							})
+					})
+			})
+	})
+
+	it('insert numberset', function(done) {
+		DynamoDB
+			.table($tableName)
+			.where('hash').eq('hash_test_numberset')
+			.where('range').eq(1)
+			.delete(function( err, data ) {
+				DynamoDB
+					.table($tableName)
+					.insert({
+						hash: 'hash_test_numberset',
+						range: 1,
+						number_set: DynamoDB.numberSet([111,222,333]),
+					}, function(err, data) {
+						if (err)
+							throw err
+
+						DynamoDB
+							.table($tableName)
+							.where('hash').eq('hash_test_numberset')
+							.where('range').eq(1)
+							.get(function(err, item) {
+								if (err)
+									throw err
+
+								item.number_set.sort()
+								assert.deepEqual(item, {
+									hash: 'hash_test_numberset',
+									range: 1,
 									number_set: [ 111, 222, 333 ],
 								}, {strict: true })
 								done()
@@ -133,6 +199,33 @@ describe('insert', function () {
 			})
 	})
 
+	it('insert binary', function(done) {
+		var $buf_obj = {
+			hash: 'hash_test_buffer',
+			range: 1,
+			buffer: new Buffer("test", "utf-8"),
+			nested_array_buffer: [new Buffer("test", "utf-8")],
+			nested_object_buffer: { buffer: new Buffer("test", "utf-8") },
+		}
+		DynamoDB
+			.table($tableName)
+			.insert($buf_obj, function(err, data) {
+				if (err)
+					throw err
+
+				DynamoDB
+					.table($tableName)
+					.where('hash').eq('hash_test_buffer')
+					.where('range').eq(1)
+					.get(function(err, item) {
+						if (err)
+							throw err
+
+						assert.deepEqual(item, $buf_obj )
+						done()
+					})
+			})
+	})
 	it('should fail when item already exists', function(done) {
 		DynamoDB
 			.table($tableName)
