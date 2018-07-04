1323ed38ab4c49c2b1944dec8dd033b95de4e93f
databank <adrian@databank.ro>
2018-07-04 15:10:13 +0300

    sql tests for UPDATE

--- test/000-index.js
+++ test/000-index.js
@@ -8,6 +8,7 @@ run_test('[SQL] INSERT',  'test/res/030-insert.yaml' ) // insert sql version
 require("./tests/031-insert_or_update.js")
 require("./tests/032-insert_or_replace.js")
 require("./tests/040-update.js")
+run_test('[SQL] UPDATE',  'test/res/040-update.yaml' ) // insert sql version
 require("./tests/050-replace.js")
 require("./tests/060-delete.js")
 require("./tests/070-get.js")
