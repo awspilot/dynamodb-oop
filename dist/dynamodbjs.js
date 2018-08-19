53431e85f3cd3c1ec447948a5be2a63dbd044f53
databank <adrian@databank.ro>
2018-08-19 10:33:20 +0300

    browser version

--- dist/dynamodbjs.js
+++ dist/dynamodbjs.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 },{}],2:[function(require,module,exports){
 /*!
@@ -25,9 +25,6 @@ function isSlowBuffer (obj) {
 
 },{}],3:[function(require,module,exports){
 (function (process){
-// .dirname, .basename, and .extname methods are extracted from Node.js v8.11.1,
-// backported and transplited with Babel, with backwards-compat fixes
-
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -78,6 +75,14 @@ function normalizeArray(parts, allowAboveRoot) {
 
   return parts;
 }
+
+// Split a filename into [root, dir, basename, ext], unix version
+// 'root' is just a slash, or nothing.
+var splitPathRe =
+    /^(\/?|)([\s\S]*?)((?:\.{1,2}|[^\/]+?|)(\.[^.\/]*|))(?:[\/]*)$/;
+var splitPath = function(filename) {
+  return splitPathRe.exec(filename).slice(1);
+};
 
 // path.resolve([from ...], to)
 // posix version
@@ -194,120 +199,37 @@ exports.relative = function(from, to) {
 exports.sep = '/';
 exports.delimiter = ':';
 
-exports.dirname = function (path) {
-  if (typeof path !== 'string') path = path + '';
-  if (path.length === 0) return '.';
-  var code = path.charCodeAt(0);
-  var hasRoot = code === 47 /*/*/;
-  var end = -1;
-  var matchedSlash = true;
-  for (var i = path.length - 1; i >= 1; --i) {
-    code = path.charCodeAt(i);
-    if (code === 47 /*/*/) {
-        if (!matchedSlash) {
-          end = i;
-          break;
-        }
-      } else {
-      // We saw the first non-path separator
-      matchedSlash = false;
-    }
+exports.dirname = function(path) {
+  var result = splitPath(path),
+      root = result[0],
+      dir = result[1];
+
+  if (!root && !dir) {
+    // No dirname whatsoever
+    return '.';
   }
 
-  if (end === -1) return hasRoot ? '/' : '.';
-  if (hasRoot && end === 1) {
-    // return '//';
-    // Backwards-compat fix:
-    return '/';
+  if (dir) {
+    // It has a dirname, strip trailing slash
+    dir = dir.substr(0, dir.length - 1);
   }
-  return path.slice(0, end);
+
+  return root + dir;
 };
 
-function basename(path) {
-  if (typeof path !== 'string') path = path + '';
 
-  var start = 0;
-  var end = -1;
-  var matchedSlash = true;
-  var i;
-
-  for (i = path.length - 1; i >= 0; --i) {
-    if (path.charCodeAt(i) === 47 /*/*/) {
-        // If we reached a path separator that was not part of a set of path
-        // separators at the end of the string, stop now
-        if (!matchedSlash) {
-          start = i + 1;
-          break;
-        }
-      } else if (end === -1) {
-      // We saw the first non-path separator, mark this as the end of our
-      // path component
-      matchedSlash = false;
-      end = i + 1;
-    }
-  }
-
-  if (end === -1) return '';
-  return path.slice(start, end);
-}
-
-// Uses a mixed approach for backwards-compatibility, as ext behavior changed
-// in new Node.js versions, so only basename() above is backported here
-exports.basename = function (path, ext) {
-  var f = basename(path);
+exports.basename = function(path, ext) {
+  var f = splitPath(path)[2];
+  // TODO: make this comparison case-insensitive on windows?
   if (ext && f.substr(-1 * ext.length) === ext) {
     f = f.substr(0, f.length - ext.length);
   }
   return f;
 };
 
-exports.extname = function (path) {
-  if (typeof path !== 'string') path = path + '';
-  var startDot = -1;
-  var startPart = 0;
-  var end = -1;
-  var matchedSlash = true;
-  // Track the state of characters (if any) we see before our first dot and
-  // after any path separator we find
-  var preDotState = 0;
-  for (var i = path.length - 1; i >= 0; --i) {
-    var code = path.charCodeAt(i);
-    if (code === 47 /*/*/) {
-        // If we reached a path separator that was not part of a set of path
-        // separators at the end of the string, stop now
-        if (!matchedSlash) {
-          startPart = i + 1;
-          break;
-        }
-        continue;
-      }
-    if (end === -1) {
-      // We saw the first non-path separator, mark this as the end of our
-      // extension
-      matchedSlash = false;
-      end = i + 1;
-    }
-    if (code === 46 /*.*/) {
-        // If this is our first dot, mark it as the start of our extension
-        if (startDot === -1)
-          startDot = i;
-        else if (preDotState !== 1)
-          preDotState = 1;
-    } else if (startDot !== -1) {
-      // We saw a non-dot and non-path separator before our dot, so we should
-      // have a good chance at having a non-empty extension
-      preDotState = -1;
-    }
-  }
 
-  if (startDot === -1 || end === -1 ||
-      // We saw a non-dot character immediately before the dot
-      preDotState === 0 ||
-      // The (right-most) trimmed path component is exactly '..'
-      preDotState === 1 && startDot === end - 1 && startDot === startPart + 1) {
-    return '';
-  }
-  return path.slice(startDot, end);
+exports.extname = function(path) {
+  return splitPath(path)[3];
 };
 
 function filter (xs, f) {
@@ -585,7 +507,8 @@ window['@awspilot/dynamodb'] = DynamoDB
 			error: function() {},
 			beforeRequest: function() {}
 		}
-
+		this.describeTables = {}
+		
 		// $config will not be an instance of DynamoDB becanse we have a different instance of AWS sdk loaded
 		// aws had similar issues in the past: https://github.com/awslabs/dynamodb-document-js-sdk/issues/16
 
@@ -773,13 +696,43 @@ window['@awspilot/dynamodb'] = DynamoDB
 		throw new Error('DELETE action is not supported by aws-dynamodb for type: ' + typeof data );
 	}
 
+	DynamoDB.prototype.addTableSchema = function( $schema ) {
+
+		if (typeof $schema !== "object")
+			throw new Error("[AWSPILOT] Invalid parameter, schema must be Array of Objects or Object");
+
+		if (! $schema.hasOwnProperty('TableName') )
+			throw new Error("[AWSPILOT] Invalid parameter, missing $schema.TableName");
+
+		if (! $schema.hasOwnProperty('KeySchema') )
+			throw new Error("[AWSPILOT] Invalid parameter, missing $schema.KeySchema");
+
+
+		this.describeTables[$schema.TableName] = $schema;
+	}
+
+	DynamoDB.prototype.schema = function( $schemas ) {
+		var $this = this;
+		if (typeof $schemas !== "object")
+			throw new Error("[AWSPILOT] Invalid parameter, schema must be Array or Object");
+		
+		if (Array.isArray($schemas)) 
+			$schemas.map(function(s) {
+				$this.addTableSchema(s)
+			})
+		else
+			this.addTableSchema($schemas)
+		
+		return this;
+	}
+
 	DynamoDB.prototype.table = function($tableName) {
-		return new Request( this.client, this.events ).table($tableName)
+		return new Request( this.client, { events: this.events, describeTables: this.describeTables } ).table($tableName)
 	}
 
 
 	DynamoDB.prototype.query = function() {
-		var r = new Request( this.client, this.events )
+		var r = new Request( this.client, { events: this.events, describeTables: this.describeTables } )
 		return r.sql(arguments[0],arguments[1]);
 	}
 
@@ -810,8 +763,9 @@ window['@awspilot/dynamodb'] = DynamoDB
 	DynamoDB.prototype.TOTAL = 'TOTAL'
 	DynamoDB.prototype.INDEXES = 'INDEXES'
 
-	function Request( $client, $events ) {
-		this.events = $events // global events
+	function Request( $client, config ) {
+		this.events = config.events // global events
+		this.describeTables = config.describeTables
 		this.local_events = {}
 		this.client = $client
 
@@ -876,6 +830,10 @@ window['@awspilot/dynamodb'] = DynamoDB
 		})
 	}
 	Request.prototype.describeTable = function( table, callback ) {
+		if (this.describeTables.hasOwnProperty(table)) {
+			return callback.apply( this, [ null, { Table: this.describeTables[table] } ] )
+		}
+		
 		this.routeCall('describeTable', { TableName: table }, false, function(err,data) {
 			return callback.apply( this, [ err, data ] )
 		})
@@ -5657,5 +5615,5 @@ DynamoUtil.normalizeValue  = DynamoUtil.parse;
 
 module.exports = DynamoUtil
 
-}).call(this,{"isBuffer":require("../../../../../../../../.nvm/versions/node/v10.8.0/lib/node_modules/browserify/node_modules/is-buffer/index.js")})
-},{"../../../../../../../../.nvm/versions/node/v10.8.0/lib/node_modules/browserify/node_modules/is-buffer/index.js":2}]},{},[5]);
+}).call(this,{"isBuffer":require("../../../../../../../../.npm/lib/node_modules/browserify/node_modules/insert-module-globals/node_modules/is-buffer/index.js")})
+},{"../../../../../../../../.npm/lib/node_modules/browserify/node_modules/insert-module-globals/node_modules/is-buffer/index.js":2}]},{},[5]);
