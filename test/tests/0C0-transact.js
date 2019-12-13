3d4b93328f00065c23d1fbe357040266bf26c6a3
adrianpraja <adrian@databank.ro>
2019-12-13 11:14:13 +0200

    +

---
+++ test/tests/0C0-transact.js
@@ -0,0 +1,113 @@
+describe('.transact()', function () {
+
+	it('.transact().table( <NONEXISTENT> )', function(done) {
+
+		DynamoDB
+			.transact()
+			.table('nonexistent').insert_or_replace({hash: 'h1', range: 1, number: 1})
+			.write(function( err, data ) {
+				//console.log(err)
+				if (err)
+					return done()
+
+				throw 'should have failed on inexistent table';
+
+			})
+	})
+
+
+
+	it('.transact().insert()', function(done) {
+
+		DynamoDB
+			.transact()
+			.table($tableName).insert({hash: 'insert', range: 1, number: 1})
+			.table($tableName).insert({hash: 'insert', range: 2, bool: true})
+			.write(function( err, data ) {
+				if (err)
+					throw err;
+
+				//console.log(err,data)
+
+
+				DynamoDB
+					.batch()
+					.table($tableName)
+					.get({hash: 'insert', range: 1,})
+					.get({hash: 'insert', range: 2,})
+					.get({hash: 'insert', range: 3,})
+					.consistent_read()
+					.read(function( err, data ) {
+						if (err)
+							throw err;
+
+						//console.log(JSON.stringify(data,null,"\t"))
+
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 1 })[0] , {
+							"range": 1,
+							"number": 1,
+							"hash": "insert"
+						})
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 2 })[0] , {
+							"bool": true,
+							"range": 2,
+							"hash": "insert"
+						})
+
+						done()
+
+					})
+
+			})
+	})
+
+
+
+	it('.transact().insert_or_replace()', function(done) {
+
+		DynamoDB
+			.transact()
+			.table($tableName).insert_or_replace({hash: 'insert_or_replace', range: 1, number: 1})
+			.table($tableName).insert_or_replace({hash: 'insert_or_replace', range: 2, bool: true})
+			.write(function( err, data ) {
+				//console.log(err,data)
+				if (err)
+					throw err;
+
+				DynamoDB
+					.batch()
+					.table($tableName)
+					.get({hash: 'insert_or_replace', range: 1,})
+					.get({hash: 'insert_or_replace', range: 2,})
+					.get({hash: 'insert_or_replace', range: 3,})
+					.consistent_read()
+					.read(function( err, data ) {
+						if (err)
+							throw err;
+
+						//console.log(JSON.stringify(data,null,"\t"))
+
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 1 })[0] , {
+							"range": 1,
+							"number": 1,
+							"hash": "insert_or_replace"
+						})
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 2 })[0] , {
+							"bool": true,
+							"range": 2,
+							"hash": "insert_or_replace"
+						})
+
+						done()
+
+					})
+
+
+
+			})
+	})
+
+
+
+
+})
