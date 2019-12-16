a485df8fb78fa95e4af66d7f899465ec72dc3334
adrianpraja <adrian@databank.ro>
2019-12-16 12:44:06 +0200

    .transact().if().replace()

--- test/tests/0C0-transact.js
+++ test/tests/0C0-transact.js
@@ -322,4 +322,79 @@ describe('.transact()', function () {
 			})
 	})
 
+
+	it('.transact().if().insert_or_replace()', function(done) {
+
+		DynamoDB
+			.transact()
+			//.table($tableName).insert_or_replace({hash: 'insert_or_replace', range: 1, number: 1})
+			//.table($tableName).insert_or_replace({hash: 'insert_or_replace', range: 2, bool: true})
+			.table($tableName)
+				.if('status').eq("updated")
+				.insert_or_replace({hash: 'insert', range: 3,  bool: true})
+			.write(function( err, data ) {
+				//console.log(err,data)
+				if (err)
+					throw err;
+
+				done()
+			})
+	})
+
+
+	it('.transact().if().replace()', function(done) {
+
+		DynamoDB
+			.transact()
+			.table($tableName).if('status').eq('replaced').replace({hash: 'insert', range: 1, status: 'if_replaced' })
+			//.table($tableName).replace({hash: 'insert', range: 2, status: 'replaced' })
+			//.table($tableName).replace({hash: 'insert', range: 3, status: 'replaced', insert_number: 99 })
+			.write(function( err, data ) {
+				// console.log(err,data)
+
+				if (err)
+					throw err;
+
+				DynamoDB
+					.batch()
+					.table($tableName)
+					.get({hash: 'insert', range: 1,})
+					.get({hash: 'insert', range: 2,})
+					.get({hash: 'insert', range: 3,})
+					.consistent_read()
+					.read(function( err, data ) {
+						if (err)
+							throw err;
+
+						//console.log(JSON.stringify(data,null,"\t"))
+
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 1 })[0] , {
+							hash: "insert",
+							range: 1,
+							status: 'if_replaced'
+						})
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 2 })[0] , {
+						// 	hash: "insert",
+						// 	range: 2,
+						// 	status: 'replaced'
+						//
+						//
+						// })
+						//
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 3 })[0] , {
+						// 	hash: "insert",
+						// 	range: 3,
+						// 	status: 'replaced',
+						// 	insert_number: 99
+						// })
+
+						done()
+
+					})
+
+			})
+	})
+
+
+
 })
