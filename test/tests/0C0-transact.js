518878fd34affc56df247c290f8edf0de253cd56
adrianpraja <adrian@databank.ro>
2019-12-17 09:43:13 +0200

    update

--- test/tests/0C0-transact.js
+++ test/tests/0C0-transact.js
@@ -122,6 +122,77 @@ describe('.transact()', function () {
 	})
 
 
+	it('.transact().if().insert_or_replace()', function(done) {
+
+		DynamoDB
+			.transact()
+			.table($tableName)
+				.if('number').eq(1)
+				.if('number').ge(1)
+				.if('number').gt(0.9)
+				.if('number').le(1)
+				.if('number').lt(1.1)
+				.if('number').ne('z')
+				.if('number').between(0.9,1.1)
+				.if('number').not().between(2,7)
+				.if('number').in( [ 0.9,1,1.1, "a", "Z" ] )
+				.if('number').not().in( [ 0.9,1.1, "a", "Z" ] )
+				.if('hash').contains('_or_')
+				//.if('hash').not_contains('range')
+				.if('hash').not().contains('range')
+				.if('hash').begins_with('insert_or')
+				.if('hash').not().begins_with('somestring')
+				.if('number').ne(null)
+				.insert_or_replace({hash: 'insert_or_replace', range: 1, number: 1})
+			// .table($tableName).insert_or_replace({hash: 'insert_or_replace', range: 2, bool: true})
+			// .table($tableName).insert_or_replace({hash: 'insert', range: 3,  bool: true})
+			.write(function( err, data ) {
+				//console.log(err,data)
+				if (err)
+					throw err;
+
+				DynamoDB
+					.batch()
+					.table($tableName)
+					.get({hash: 'insert_or_replace', range: 1,})
+					// .get({hash: 'insert_or_replace', range: 2,})
+					// .get({hash: 'insert', range: 3,})
+					.consistent_read()
+					.read(function( err, data ) {
+						if (err)
+							throw err;
+
+						//console.log(JSON.stringify(data,null,"\t"))
+
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 1 })[0] , {
+							"range": 1,
+							"number": 1,
+							"hash": "insert_or_replace"
+						})
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 2 })[0] , {
+						// 	"bool": true,
+						// 	"range": 2,
+						// 	"hash": "insert_or_replace"
+						// })
+						//
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 3 })[0] , {
+						// 	"bool": true,
+						// 	"range": 3,
+						// 	"hash": "insert"
+						// })
+
+						done()
+
+					})
+
+
+
+			})
+	})
+
+
+
+
 	it('.transact().replace()', function(done) {
 
 		DynamoDB
@@ -168,6 +239,65 @@ describe('.transact()', function () {
 							status: 'replaced',
 							insert_number: 99
 						})
+
+						done()
+
+					})
+
+			})
+	})
+
+
+	it('.transact().if().replace()', function(done) {
+
+		DynamoDB
+			.transact()
+			.table($tableName)
+				.if('status').eq('replaced')
+				.if('range').gt(0.9)
+				.if('range').lt(1.1)
+				.replace({hash: 'insert', range: 1, status: 'replaced' })
+
+			//.table($tableName).replace({hash: 'insert', range: 2, status: 'replaced' })
+			//.table($tableName).replace({hash: 'insert', range: 3, status: 'replaced', insert_number: 99 })
+			.write(function( err, data ) {
+				// console.log(err,data)
+
+				if (err)
+					throw err;
+
+				DynamoDB
+					.batch()
+					.table($tableName)
+					.get({hash: 'insert', range: 1,})
+					// .get({hash: 'insert', range: 2,})
+					// .get({hash: 'insert', range: 3,})
+					.consistent_read()
+					.read(function( err, data ) {
+						if (err)
+							throw err;
+
+						//console.log(JSON.stringify(data,null,"\t"))
+
+						assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 1 })[0] , {
+							hash: "insert",
+							range: 1,
+							status: 'replaced'
+						})
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 2 })[0] , {
+						// 	hash: "insert",
+						// 	range: 2,
+						// 	status: 'replaced'
+						//
+						//
+						// })
+						//
+						// assert.deepEqual( data[$tableName].filter(function(d) {return d.range === 3 })[0] , {
+						// 	hash: "insert",
+						// 	range: 3,
+						// 	status: 'replaced',
+						// 	insert_number: 99
+						// })
 
 						done()
 
